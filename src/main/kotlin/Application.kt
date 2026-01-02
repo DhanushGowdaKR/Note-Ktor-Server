39aa2a49987bf7942626db79222a65399cda3882
DhanushGowdaKR <dhanushgowdakr18@gmail.com>
2026-01-02 08:23:19 +0530

    Fix port error

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -6,13 +6,18 @@ import com.dhanush.route.noteRoute
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.sse.SSE
 import kotlinx.serialization.json.Json
 import org.koin.ktor.ext.inject
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    embeddedServer(
+        Netty,
+        port = System.getenv("PORT")?.toInt() ?: 8080
+    ) { module() }
 }
 
 fun Application.module() {
